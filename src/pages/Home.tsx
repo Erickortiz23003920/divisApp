3c9d556bc7840bfe8e143feec8e09553e40ed23c
Erickortiz23003920 <124820684+Erickortiz23003920@users.noreply.github.com>
2024-11-22 23:21:25 -0600

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React, { useState } from 'react';
+import { IonContent, IonInput, IonItem, IonLabel, IonButton, IonText, IonTitle } from '@ionic/react';
+import './Home.css'
 
 const Home: React.FC = () => {
+  const [quetzales, setQuetzales] = useState<number>();
+  const [dolares, setDolares] = useState<number>();
+
+  const convertir = () => {
+    if (quetzales !== undefined) {
+      setDolares(quetzales / 8);
+    }
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <IonContent className="ion-padding">
+      <IonItem>
+        <h1> DivisApp 23003920</h1>
+        <IonLabel position="floating">Cantidad en Quetzales</IonLabel>
+        <IonInput type="number" value={quetzales} onIonChange={e => setQuetzales(parseFloat(e.detail.value!))}></IonInput>
+      </IonItem>
+      <IonButton expand="full" onClick={convertir}>Convertir</IonButton>
+      {dolares !== undefined && (
+        <IonText>
+          <h2>Equivalente en Dólares: ${dolares.toFixed(2)}</h2>
+        </IonText>
+      )}
+    </IonContent>
   );
 };
 
